561e03ce9b13ab2b7b815aa096fe589d371928eb
Monish Basaniwal <monish2.basaniwal@gmail.com>
2023-04-25 00:51:33 +0530

    Changed number of pages

--- review_scraper.py
+++ review_scraper.py
@@ -26,7 +26,7 @@ no_reviews=html_soup.find("div", {"class" : "paginationFooter"})
 potential_num = no_reviews.get_text().split(" ")[-2]
 if "," in potential_num:
     potential_num = math.floor(int(potential_num.replace(",","")) /10)
-potential_num = potential_num if potential_num <= 3 else 3
+potential_num = potential_num if potential_num <= 10 else 10
 urls = []
 base_split = base_url.split(".htm")
 urls.append(base_url)
